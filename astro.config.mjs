349a0105eb1d54087554c3b919d999907b3ced77
Cesare Montedonico <cmontedonico@yahoo.com>
2024-10-06 16:47:56 -0600

    Adding Tailwind, Sentry

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,16 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+
+import sentry from "@sentry/astro";
+import spotlightjs from "@spotlightjs/astro";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  server: {
+    port: 3000,
+  },
+
+  integrations: [sentry(), spotlightjs(), tailwind()],
+});
